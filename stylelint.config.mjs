6f8fda5dd75e638ae6d90d9da3fefef0a8b5bd08
Muhammad Talal Anwar <noreply@talal.ch>
2026-08-01 02:40:55 +0200

    style(CSS): use stylelint and fix issues

---
+++ stylelint.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: ["stylelint-config-standard"],
+  rules: {
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: [
+          "tailwind",
+          "apply",
+          "variants",
+          "responsive",
+          "screen",
+          "layer"
+        ]
+      }
+    ],
+    "no-descending-specificity": null,
+    "block-no-empty": null,
+    "selector-class-pattern": null,
+    "property-no-vendor-prefix": null,
+    "keyframes-name-pattern": null,
+    "selector-id-pattern": null,
+    "declaration-property-value-keyword-no-deprecated": null,
+    "declaration-property-value-no-unknown": null
+  }
+};
